59f607b91ef1dcf1dde8575098794ea1e8695c38
Mateusz Adamowski <m.adamowski@alx.pl>
2021-11-09 18:55:02 +0100

    listy - przykład

---
+++ 2021-11-09/przyklad1.py
@@ -0,0 +1,13 @@
+# typ danych: lista (ang. list)
+oceny = [5, 6, 4, 6, 6, 6, 5, 4]
+print(f"{oceny=}")
+print(f"{type(oceny)=}")
+
+miesiace = [None, "styczeń", "luty", "marzec", "kwiecień"]
+print(f"{miesiace=}")
+
+print(f"{oceny[0]=}")
+print(f"{miesiace[2]=}")
+
+nr_miesiaca = 3
+print(f"{miesiace[nr_miesiaca]=}")
