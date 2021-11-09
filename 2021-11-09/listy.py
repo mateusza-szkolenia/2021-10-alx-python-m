99be4fbe58cfa383adbff01ae79a6aab7daf0ad7
Mateusz Adamowski <m.adamowski@alx.pl>
2021-11-09 19:18:21 +0100

    listy - indeksowanie

---
+++ 2021-11-09/listy.py
@@ -0,0 +1,18 @@
+uczniowie = ["Ala", "Basia", "Czesia", "Dorota", "Ewa", "Frania", "Grażyna", "Halina", "Iwona", "Justyna"]
+
+print(f"{uczniowie[4]=}")
+
+# len() czyli length, czyli długość
+print(f"{len(uczniowie)=}")
+
+# ostatnia:
+a = len(uczniowie)
+print(f"{uczniowie[a-1]=}")
+
+print(f"{uczniowie[len(uczniowie)-1]=}")
+
+print(f"{uczniowie[-1]=}")
+
+# trzecia od końca
+print(f"{uczniowie[-3]=}")
+
