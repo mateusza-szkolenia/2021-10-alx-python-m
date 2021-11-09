2db55b18ade35b7ad2e2ce681a4aeb810186dd7d
Mateusz Adamowski <m.adamowski@alx.pl>
2021-11-09 19:24:23 +0100

    stringi - indeksowanie

---
+++ 2021-11-09/stringi.py
@@ -0,0 +1,18 @@
+#
+#    0   1   2   3   4   5   6
+#    M   a   t   e   u   s   z
+#   -7  -6  -5  -4  -3  -2  -1
+
+imie = "Mateusz"
+
+print(f"{imie[0]=}")
+print(f"{imie[1]=}")
+print(f"{imie[4]=}")
+print(f"{imie[-1]=}")
+print(f"{imie[-7]=}")
+
+# Tak nie wolno, bo będzie błąd (indeks poza zakresem)
+# print(f"{imie[100]=}")
+# print(f"{imie[-100]=}")
+
+
